b518681f704149c245b6f6f1fcd204918c7587fc
Icefly <Icefly@users.noreply.github.com>
2014-05-20 14:20:48 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,24 +1,30 @@
-## Put comments here that give an overall description of what your
+## makeCacheMatrix function that generates a set of functions
+## get the value
 makeCacheMatrix <- function(x=matrix()){
-  sol <- NULL
+  sol <- NULL  ## sol variable will be where the inverse matrix is saved.
   set <- function(y){
     x <<- y
     sol <<- NULL
   }
   get <- function()x
-  setreverse <- function(solve) sol<<-solve
-  getreverse <- function() sol
-  list(set=set, get=get, setreverse=setreverse, getreverse=getreverse)
+  setinverse <- function(solve) sol<<-solve  ## calculates the inverse matrix
+  getinverse <- function() sol ## get the cached data
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
   
-cacheSolve <- function(x, mat=matrix()) {
-  sol <- x$getreverse()
-  if (!is.null(sol)) {                      
-    message ("will get the chached calculation result")
-    return(sol)
+cacheSolve <- function(x) {  
+  ## x is the result of makeCacheMatrix
+  ## for example, if you ran the makeCacheMatix; result<-makeCacheMatrix(matrix)
+  ## then it will be cacheSolve(result)
+  
+  sol <- x$getinverse()  ## retrieve the inverse matrix value calculated in the 
+                         ## prior function and save in sol
+  if (!is.null(sol)) {   ## if sol has value, in other words, it was calculated before,                  
+    message ("will get the cached result")
+    return(sol)         ## use the prior calculation result
   }
-  data = x$get()
-  sol <- solve(mat)
-  x$setreverse(sol)
+  data = x$get()        ## if not previously calculated, get the matrix
+  sol <- solve(data)    ## and calculate the inverse matrix
+  x$setinverse(sol)
   sol
 }
